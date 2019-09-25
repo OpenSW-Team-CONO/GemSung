64be63d6994d3f57ecdf01591bef953e60560438
KwonDongHyung <kdhhyunjin1@gmail.com>
2019-09-25 20:54:08 +0900

    First Map
    Update App.js

--- gemsung/App.js
+++ gemsung/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {MapView} from 'expo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <MapView
+        style={{flex:1}}
+      />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

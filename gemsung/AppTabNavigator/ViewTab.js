c470a5662a2703d2de758075c7f1ca10da54daf1
KwonDongHyung <kdhhyunjin1@gmail.com>
2019-10-05 17:15:04 +0900

    ViewTab 미완성

--- gemsung/AppTabNavigator/ViewTab.js
+++ gemsung/AppTabNavigator/ViewTab.js
@@ -1,6 +1,58 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Icon } from 'native-base';
+import {Alert} from "react-native"
+import MapView from 'react-native-maps';
+import * as Location from "expo-location";
+import CreateTab from './CreateTab';
+/*
+export default class App extends React.Component {
+  state={
+    region:{
+      latitude:35,
+      longitude:127,
+      latitudeDelta:0.0922,
+      longitudeDelta:0.0421
+    }
+  }
+  getLocation=async()=>{
+    try{
+      await Location.requestPermissionsAsync();
+      const {
+        coords
+      } = await Location.getCurrentPositionAsync();
+      this.setState({
+        region:{
+          latitude:coords.latitude,
+          longitude:coords.longitude,
+          latitudeDelta:0.0922,
+          longitudeDelta:0.0421
+        }
+      });
+    } catch(error){
+      Alert.alert("Can't find you.","So sad");
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  onRegionChange=(region)=>{
+    this.setState({region});
+  }
+  render() {
+      return (
+      <MapView
+       style={{flex: 1}}
+       initialRegion={this.state.region}
+       region={this.state.region}
+       onRegionChange={this.onRegionChange}
+      />
+    );
+  }
+}*/
+
+let lo;
+var lat, long;
 
 export default class ViewTab extends Component {
   static navigationOptions = {
@@ -8,15 +60,48 @@ export default class ViewTab extends Component {
            <Icon name='arrow-dropright-circle' style={{ color: tintColor }} />
        )
    }
+   state={
+     region:{
+       latitude:35,
+       longitude:127,
+       latitudeDelta:0.0922,
+       longitudeDelta:0.0421
+     }
+   }
+   getLocation=async()=>{
+     const { navigation } = this.props;
+     lo=JSON.stringify(navigation.getParam('photos_loc'));
+     if(lo){
+       lat=navigation.state.params.photos_loc.latitude;
+       long=navigation.state.params.photos_loc.longitude;
+       console.log(lat, long);
+       this.setState({
+       region:{
+         latitude:lat,
+         longitude:long,
+         latitudeDelta:0.0922,
+         longitudeDelta:0.0421,
+       }
+     });
+   }
+ }
+   onRegionChange=(region)=>{
+     this.setState({region});
+   }
+   componentDidMount(){
+     this.getLocation();
+     console.log(1);
+   }
     render() {
-      const { navigation } = this.props;
-        return (
-          console.log(JSON.stringify(navigation.getParam('photos_loc'))),
-            <View style={style.container}>
-                <Text>{JSON.stringify(navigation.getParam('photos_loc'))}</Text>
-            </View>
+      return (
+        <MapView
+          style={{flex: 1}}
+          initialRegion={this.state.region}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+          />
         );
-    }
+  }
 }
 
 const style = StyleSheet.create({

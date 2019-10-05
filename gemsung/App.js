e9db07fe7a2e32cc76b2062fb15809b9c9bc0fc8
KwonDongHyung <kdhhyunjin1@gmail.com>
2019-10-05 15:06:54 +0900

    update files

--- gemsung/App.js
+++ gemsung/App.js
@@ -1,49 +1,12 @@
-import React from 'react';
-import {Alert} from "react-native"
-import MapView from 'react-native-maps';
-import * as Location from "expo-location";
+import React, { Component } from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Main from './Main';
 
-export default class App extends React.Component {
-  state={
-    region:{
-      latitude:35,
-      longitude:127,
-      latitudeDelta:0.0922,
-      longitudeDelta:0.0421
-    }
+const AppStackNavigator = createStackNavigator({
+  Main:{
+    screen: Main
   }
-  getLocation=async()=>{
-    try{
-      await Location.requestPermissionsAsync();
-      const {
-        coords
-      } = await Location.getCurrentPositionAsync();
-      this.setState({
-        region:{
-          latitude:coords.latitude,
-          longitude:coords.longitude,
-          latitudeDelta:0.0922,
-          longitudeDelta:0.0421
-        }
-      });
-    } catch(error){
-      Alert.alert("Can't find you.","So sad");
-    }
-  }
-  componentDidMount(){
-    this.getLocation();
-  }
-  onRegionChange=(region)=>{
-    this.setState({region});
-  }
-  render() {
-      return (
-      <MapView
-       style={{flex: 1}}
-       initialRegion={this.state.region}
-       region={this.state.region}
-       onRegionChange={this.onRegionChange}
-      />
-    );
-  }
-}
+});
+
+export default createAppContainer(AppStackNavigator);

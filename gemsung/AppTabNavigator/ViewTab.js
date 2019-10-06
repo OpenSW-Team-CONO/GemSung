94fc87eaea548ef7cd97f4bdd12f54c0749e93c2
KwonDongHyung <kdhhyunjin1@gmail.com>
2019-10-06 14:21:22 +0900

    Viewtab delay제외 완성

--- gemsung/AppTabNavigator/ViewTab.js
+++ gemsung/AppTabNavigator/ViewTab.js
@@ -4,9 +4,20 @@ import { Icon } from 'native-base';
 import {Alert} from "react-native"
 import MapView from 'react-native-maps';
 import * as Location from "expo-location";
-import CreateTab from './CreateTab';
-/*
-export default class App extends React.Component {
+
+var id=0;
+let lo;
+let lat=[];
+let long=[];
+var i=0;
+const timer=require('react-native-timer');
+
+export default class ViewTab extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name='arrow-dropright-circle' style={{ color: tintColor }} />
+    )
+  }
   state={
     region:{
       latitude:35,
@@ -15,99 +26,43 @@ export default class App extends React.Component {
       longitudeDelta:0.0421
     }
   }
-  getLocation=async()=>{
-    try{
-      await Location.requestPermissionsAsync();
-      const {
-        coords
-      } = await Location.getCurrentPositionAsync();
+
+  onRegionChange=(region)=>{
+    for(i=0; i<id; i++){
       this.setState({
         region:{
-          latitude:coords.latitude,
-          longitude:coords.longitude,
+          latitude:lat[i],
+          longitude:long[i],
           latitudeDelta:0.0922,
           longitudeDelta:0.0421
         }
       });
-    } catch(error){
-      Alert.alert("Can't find you.","So sad");
     }
   }
-  componentDidMount(){
-    this.getLocation();
-  }
-  onRegionChange=(region)=>{
-    this.setState({region});
-  }
-  render() {
-      return (
+
+  render(){
+    const {navigation} = this.props;
+    lo=JSON.stringify(navigation.getParam('photos_loc'));
+    if(lo){
+      lat[id]=navigation.state.params.photos_loc.latitude;
+      long[id]=navigation.state.params.photos_loc.longitude;
+      id++;
+    }
+    return (
       <MapView
-       style={{flex: 1}}
-       initialRegion={this.state.region}
-       region={this.state.region}
-       onRegionChange={this.onRegionChange}
+      style={{flex: 1}}
+      initialRegion={this.state.region}
+      region={this.state.region}
+      onRegionChange={this.onRegionChange}
       />
     );
-  }
-}*/
-
-let lo;
-var lat, long;
-
-export default class ViewTab extends Component {
-  static navigationOptions = {
-       tabBarIcon: ({ tintColor }) => (
-           <Icon name='arrow-dropright-circle' style={{ color: tintColor }} />
-       )
-   }
-   state={
-     region:{
-       latitude:35,
-       longitude:127,
-       latitudeDelta:0.0922,
-       longitudeDelta:0.0421
-     }
-   }
-   getLocation=async()=>{
-     const { navigation } = this.props;
-     lo=JSON.stringify(navigation.getParam('photos_loc'));
-     if(lo){
-       lat=navigation.state.params.photos_loc.latitude;
-       long=navigation.state.params.photos_loc.longitude;
-       console.log(lat, long);
-       this.setState({
-       region:{
-         latitude:lat,
-         longitude:long,
-         latitudeDelta:0.0922,
-         longitudeDelta:0.0421,
-       }
-     });
-   }
- }
-   onRegionChange=(region)=>{
-     this.setState({region});
-   }
-   componentDidMount(){
-     this.getLocation();
-     console.log(1);
-   }
-    render() {
-      return (
-        <MapView
-          style={{flex: 1}}
-          initialRegion={this.state.region}
-          region={this.state.region}
-          onRegionChange={this.onRegionChange}
-          />
-        );
   }
 }
 
 const style = StyleSheet.create({
   container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });

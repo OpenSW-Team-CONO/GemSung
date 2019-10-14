af2c7b503b1b1d16a5e9a4ddb62ef99609915a29
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-10-14 17:04:45 +0900

    Update Main.js

--- gemsung/Main.js
+++ gemsung/Main.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { View,Text,StyleSheet, Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import CreateTab from './AppTabNavigator/CreateTab'
 import ViewTab from './AppTabNavigator/ViewTab'
+
 
 const AppTabNavigator = createMaterialTopTabNavigator({
   CreateTab: { screen: CreateTab },
@@ -37,12 +39,14 @@ const AppTabContainet = createAppContainer(AppTabNavigator);
 export default class Main extends Component {
   static navigationOptions = {
     title: 'Gemsung',
-     headerTitleStyle: {
+    headerTitleStyle: {
         textAlign:"center",
+        color:'#4C64FF',
+        fontSize:30,
+        fontFamily:'',
         flex:1
     },
   }
-
   render() {
     return (
       <AppTabContainet />
